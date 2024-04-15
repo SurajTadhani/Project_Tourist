46aa13651b24c55140e998cdb5540fd973fc73ee
Suraj <surajtadhani@gmail.com>
2024-04-15 22:30:35 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#86B817",
+        paragraph: "#808B8D",
+      },
+    },
+  },
+  plugins: [],
+};
